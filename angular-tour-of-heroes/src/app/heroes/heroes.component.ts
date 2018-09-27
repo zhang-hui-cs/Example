a0ebe29f428a0fae8acd8aeaa29037ea36209076
admin <zhang_hui_cn@126.com>
2018-09-27 23:04:40 +0800

    Angular-5.x 教程

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import { HEROES } from '../mock/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  constructor() {
+  }
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  ngOnInit() {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
